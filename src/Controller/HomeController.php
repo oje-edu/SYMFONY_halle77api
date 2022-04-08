d9484d14d77aea4ecc740f5d94a19a6dc166abb1
oje-edu <thorsten.oje@gfn.education>
2022-04-08 17:32:32 +0200

    frontpage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,13 +7,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
     public function index(Request $request): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
